ca92e28a7d7edc96ac542338d71503e08698dac5
FordPrfkt <fordprfkt@googlemail.com>
2010-09-15 23:50:21 +0200

    Work in Progress

--- services/gcalendar/gcalendar.c
+++ services/gcalendar/gcalendar.c
@@ -20,57 +20,398 @@
  */
 
 #include <avr/pgmspace.h>
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "config.h"
-#include "appsample.h"
-
+#include "gcalendar.h"
+#include "protocols/uip/uip.h"
+#include "protocols/dns/resolv.h"
+#include "protocols/ecmd/parser.h"
 #include "protocols/ecmd/ecmd-base.h"
 
-/*
-  If enabled in menuconfig, this function is called during boot up of ethersex
-*/
-int16_t
-app_sample_init(void)
+#include "../glcdmenu/glcdmenu.h"
+#include "../glcdmenu/menu-interpreter/menu-interpreter-config.h"
+
+#ifndef DNS_SUPPORT
+#error "gCalendar needs DNS support"
+#endif
+
+#define STATE (&uip_conn->appstate.gcalendar)
+#define GCALENDAR_HOST "www.google.de"
+
+static const char PROGMEM FEED_TAG = "feed";
+static const char PROGMEM ENTRY_TAG = "entry";
+static const char PROGMEM TITLE_TAG = "title type='text'";
+static const char PROGMEM WHEN_TAG = "gd:when";
+
+static const char PROGMEM REQUEST[] =
+"GET /calendar/feeds/%s/full HTTP/1.1\nHost: "GCALENDAR_HOST"\nConnection: Close\r\n\r\n";
+
+static uip_conn_t *gcalendar_conn;
+
+static void gcalendarQueryCB_v(char *name, uip_ipaddr_t *ipaddr);
+static void gcalendarMain_v(void);
+
+bool gweatherGetAttribute_b(char* inStr_pc, uint8_t inLen_ui8, char* outStr_pc, uint8_t outLen_ui8)
 {
-  APPSAMPLEDEBUG ("init\n");
-  // enter your code here
+	uint8_t pos1 = -1;
+	uint8_t pos2 = -1;
+	uint8_t size_ui8 = 0;
+	uint8_t seekPos_ui8 = 0;
 
-  return ECMD_FINAL_OK;
+	/* Seek for text within "" */
+	while (++seekPos_ui8 < inLen_ui8)
+	{
+		if (inStr_pc[seekPos_ui8] == '"')
+		{
+			pos1 = seekPos_ui8;
+			break;
+		}
+	}
+
+	seekPos_ui8 = inLen_ui8;
+	while (seekPos_ui8-- > 0)
+	{
+		if (inStr_pc[seekPos_ui8] == '"')
+		{
+			pos2 = seekPos_ui8;
+			break;
+		}
+	}
+
+	/* Text found ? */
+	if ((pos1 != -1) && (pos2 != -1) && (pos2 > pos1) && (outLen_ui8 > 0))
+	{
+		/* Copy text */
+		size_ui8 = (pos2 - pos1) - 1;
+
+		if (size_ui8 >= outLen_ui8)
+			size_ui8 = outLen_ui8 - 1;
+
+		if (size_ui8 > 0)
+		{
+			memcpy(outStr_pc, inStr_pc + (pos1 + 1), size_ui8);
+			outStr_pc[size_ui8 + 1] = 0;
+		}
+	}
+	else
+	{
+		printf("Could not get Attribute: %s\n", inStr_pc);
+	}
+
+	return ((size_ui8 > 0) ? true : false);
 }
 
-/*
-  If enabled in menuconfig, this function is periodically called
-  change "timer(100,app_sample_periodic)" if needed
-*/
-int16_t
-app_sample_periodic(void)
+bool gcalendarParse_b(char* data_pc, uint16_t len_ui16)
 {
-  APPSAMPLEDEBUG ("periodic\n");
-  // enter your code here
+	uint8_t pos_ui8 = 0;
+	bool result_b = false;
 
-  return ECMD_FINAL_OK;
+	while (pos_ui8 < len)
+	{
+		switch (elementParserState_e)
+		{
+			case ELEMPARSER_WAIT_BEGIN:
+			  if (buffer[pos_ui8] == '<')
+			  {
+				  memset(currentElement_ac, 0, sizeof(currentElement_ac));
+				  elementParserState_e = ELEMPARSER_IN_ELEMENT;
+				  elemPos_ui8 = 0;
+			  }
+			break;
+
+			case ELEMPARSER_IN_ELEMENT:
+				if (buffer[pos_ui8] != '>')
+				{
+					if (elemPos_ui8 < sizeof(currentElement_ac))
+					{
+						currentElement_ac[elemPos_ui8] = buffer[pos_ui8];
+						elemPos_ui8++;
+					}
+				}
+				else
+				{
+					elementParserState_e = ELEMPARSER_DONE;
+				}
+			break;
+
+			case ELEMPARSER_GET_LABEL:
+				if (buffer[pos_ui8] != '<')
+				{
+					if (elemPos_ui8 < sizeof(currentElement_ac))
+					{
+						currentElement_ac[elemPos_ui8] = buffer[pos_ui8];
+						elemPos_ui8++;
+					}
+				}
+				else
+				{
+					elementParserState_e = ELEMPARSER_DONE;
+					pos_ui8--;
+				}
+			break;
+
+			case ELEMPARSER_DONE:
+				elementParserState_e = ELEMPARSER_WAIT_BEGIN;
+				pos_ui8--; // Reparse the current character;
+			break;
+		}
+
+		if (ELEMPARSER_DONE == elementParserState_e)
+		{
+			switch(parserState_e)
+			{
+				case PARSER_WAIT_TAG:
+				 if (strcmp(currentElement_ac, ENTRY_TAG) == 0)
+				 {
+					parserState_e = PARSER_IN_ENTRY;
+//					printf("Entry Tag: %s\n", currentElement_ac);
+					fcPos_ui8 = 0;
+				 }
+				 else
+				 if (strcmp(currentElement_ac, "/feed") == 0)
+				 {
+					parserState_e = PARSER_DONE;
+//					printf("End Tag: %s\n", currentElement_ac);
+					fcPos_ui8 = 0;
+				 }
+				break;
+
+				case PARSER_IN_ENTRY:
+				 if (strstr(currentElement_ac, TITLE_TAG) != 0)
+				 {
+//					 printf("Ttitle Tag: %s\n", currentElement_ac);
+					 parserState_e = PARSER_IN_TITLE;
+					 elementParserState_e = ELEMPARSER_GET_LABEL;
+ 				     memset(currentElement_ac, 0, sizeof(currentElement_ac));
+				     elemPos_ui8 = 0;
+				 }
+				 else
+				 if (strcmp(currentElement_ac, "/gd:when") == 0)
+				 {
+//					 printf("End when Tag: %s\n", currentElement_ac);
+				 }
+				 else
+				 if (strstr(currentElement_ac, WHEN_TAG) != 0)
+				 {
+//					 printf("When Tag: %s\n", currentElement_ac);
+					 memcpy(text_ac+textPos_ui8, currentElement_ac+17, 10);
+					 textPos_ui8 += 10;
+					 text_ac[textPos_ui8] = ' ';
+					 textPos_ui8++;
+					 memcpy(text_ac+textPos_ui8, currentElement_ac+28, 8);
+					 textPos_ui8 += 8;
+					 text_ac[textPos_ui8] = '\n';
+					 textPos_ui8++;
+					 text_ac[textPos_ui8] = '\n';
+					 textPos_ui8++;
+				 }
+				 else
+				 if (strcmp(currentElement_ac, "/entry") == 0)
+				 {
+	//				printf("End entry Tag: %s\n", currentElement_ac);
+					parserState_e = PARSER_WAIT_TAG;
+				 }
+				 else
+				 {
+					 //printf("Unknown Tag: %s\n", currentElement_ac);
+				 }
+				break;
+
+				case PARSER_IN_TITLE:
+				 if (strcmp(currentElement_ac, "/title") == 0)
+				 {
+		//			printf("End title Tag: %s\n", currentElement_ac);
+					parserState_e = PARSER_IN_ENTRY;
+				 }
+				 else
+				 {
+//					 printf("Title: %s\n", currentElement_ac);
+					 memcpy(text_ac+textPos_ui8, currentElement_ac, elemPos_ui8);
+					 textPos_ui8 += elemPos_ui8;
+					 text_ac[textPos_ui8] = '\n';
+					 textPos_ui8++;
+				 }
+				break;
+
+				case PARSER_DONE:
+				 printf("Done\n");
+				 result_b = true;
+				break;
+
+				default:
+					printf("Oops! Dunno what to do!\n");
+				break;
+			}
+		}
+
+		pos_ui8++;
+	}
+
+	return result_b;
+}
+
+void gcalendarSendRequest_v(void)
+{
+	uint16_t len_ui16;
+	len_ui16 = sprintf_P(uip_sappdata, REQUEST, gCalendarLogin_ac);
+	GCALENDARDEBUG("Send GET request(%i): %s\n", len_ui16, ((char*)uip_sappdata));
+	uip_send(uip_sappdata, len_ui16);
+}
+
+static void gcalendarQueryCB_v(char *name, uip_ipaddr_t *ipaddr)
+{
+	if (NULL == ipaddr)
+	{
+		GCALENDARDEBUG ("Could not resolve address\n");
+	}
+	else
+	{
+		GCALENDARDEBUG ("Address resolved\n");
+		gcalendar_conn = uip_connect(ipaddr, HTONS (80), gcalendarMain_v);
+
+		if (NULL != gweather_conn)
+		{
+			GCALENDARDEBUG ("Wait for connect\n");
+			STATE->stage_e = GCALENDAR_CONNECT;
+		}
+		else
+		{
+			GCALENDARDEBUG ("no uip_conn available.\n");
+		}
+	}
+}
+
+bool gcalendarSetLogin_b(char* login_pc, uint16_t len_ui16)
+{
+	if (len_ui16 > GCALENDAR_LOGINSIZE)
+		return false;
+
+	memcpy(login_pc, gCalendarLogin_ac, len_ui16);
+
+#ifdef GCALENDAR_EEPROM_SUPPORT
+	eeprom_save(gCalendarLogin_ac, gCalendarLogin_ac, GCALENDAR_LOGINSIZE);
+	eeprom_update_chksum();
+#endif
+
+	return true;
+}
+
+int16_t gcalendarUpdate_i16(char *cmd_pc, char *output_pc, uint16_t len_ui16)
+{
+	uip_ipaddr_t* ip_p;
+
+	GCALENDARDEBUG ("updating.\n");
+
+	ip_p = resolv_lookup(GCALENDAR_HOST);
+
+	if (NULL == ip_p)
+	{
+		GCALENDARDEBUG ("Resolving Address\n");
+		resolv_query(GCALENDAR_HOST, gweatherQueryCB_v);
+	}
+	else
+	{
+		GCALENDARDEBUG ("address already resolved\n");
+		gweather_conn = uip_connect(ip_p, HTONS(80), gweatherMain_v);
+
+		if (NULL == gweather_conn)
+		{
+			GCALENDARDEBUG ("no uip_conn available.\n");
+		}
+		else
+		{
+			GCALENDARDEBUG ("Wait for connect\n");
+			STATE->stage_e = GCALENDAR_CONNECT;
+		}
+
+		return ECMD_FINAL_OK;
+	}
+
+	return ECMD_FINAL_OK;
+}
+
+static void gcalendarMain_v(void)
+{
+	if (uip_aborted() || uip_timedout())
+	{
+		GCALENDARDEBUG ("connection aborted\n");
+		gweather_conn = NULL;
+	}
+
+	if (uip_closed())
+	{
+		GCALENDARDEBUG("connection closed\n");
+		gweather_conn = NULL;
+		glcdmenuRedraw();
+	}
+
+	if (uip_connected() && STATE->stage_e == GCALENDAR_CONNECT)
+	{
+		GCALENDARDEBUG("Connected\n");
+		STATE->stage_e = GCALENDAR_SEND_REQUEST;
+	}
+
+	if (uip_rexmit() && (STATE->stage_e == GCALENDAR_SEND_REQUEST
+			|| STATE->stage_e == GCALENDAR_WAIT_RESPONSE))
+	{
+		GCALENDARDEBUG("Re-Xmit\n");
+		gweatherSendRequest_v();
+		STATE->stage_e = GCALENDAR_WAIT_RESPONSE;
+	}
+
+	if (uip_poll() && (STATE->stage_e == GCALENDAR_SEND_REQUEST))
+	{
+		gweatherSendRequest_v();
+		STATE->stage_e = GCALENDAR_WAIT_RESPONSE;
+	}
+
+	if (uip_acked() && STATE->stage_e == GCALENDAR_WAIT_RESPONSE)
+	{
+		GCALENDARDEBUG("Request ACKed\n");
+		STATE->stage_e = GCALENDAR_RECEIVE;
+		STATE->parserState_e = PARSER_WAIT_START;
+		STATE->elementParserState_e = ELEMPARSER_WAIT_BEGIN;
+
+		memset(text_ac, 0, sizeof(text_ac));
+		//glcdmenuSetString(MENU_TEXT_W_CITY, (unsigned char*) city_ac);
+	}
+
+	if (uip_newdata())
+	{
+		if (uip_len && STATE->stage_e == GCALENDAR_RECEIVE)
+		{
+			GCALENDARDEBUG("New Data\n");
+			if (gweatherParse_b(((char *) uip_appdata), uip_len) == false)
+			{
+				GCALENDARDEBUG("Parser error\n");
+				uip_close (); /* Parse error */
+				return;
+			}
+		}
+	}
+}
+
+void gcalendarInit_v(void)
+{
+	GCALENDARDEBUG("initializing google calendar client\n");
+
+#ifdef GCALENDAR_EEPROM_SUPPORT
+	eeprom_restore(gCalendarLogin_ac, &gCalendarLogin_ac, GCALENDAR_LOGINSIZE);
+#else
+	sprintf(gCalendarLogin_ac, PSTR("%s"), CONF_GCALENDAR_LOGIN);
+#endif
 }
 
 /*
-  This function will be called on request by menuconfig, if wanted...
-  You need to enable ECMD_SUPPORT for this.
-  Otherwise you can use this function for anything you like 
-*/
-int16_t
-app_sample_onrequest(char *cmd, char *output, uint16_t len){
-  APPSAMPLEDEBUG ("main\n");
-  // enter your code here
+ -- Ethersex META --
+ header(services/gcalendar/gcalendar.h)
+ net_init(gcalendarInit_v)
+ state_header(services/gcalendar/gcalendar_state.h)
+ state_tcp(struct gcalendar_connection_state_t gcalendar)
+ */
 
-  return ECMD_FINAL_OK;
-}
-
-/*
-  -- Ethersex META --
-  header(services/appsample/appsample.h)
-  ifdef(`conf_APP_SAMPLE_INIT_AUTOSTART',`init(app_sample_init)')
-  ifdef(`conf_APP_SAMPLE_PERIODIC_AUTOSTART',`timer(100,app_sample_periodic())')
-*/
+/* EOF */
